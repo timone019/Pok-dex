66cb7a69bf0dc13bf254334840400cbf830b940d
timone019 <143154960+timone019@users.noreply.github.com>
2023-10-01 23:54:23 -0700

    created pokemon array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert('Hello world');
-let favoriteFood = 'burgermaster';
-document.write(favoriteFood);
+let pokemonList = [
+    { name: "Charmander", height: .6, type: ["fire"] },
+    { name: "Squirtle", height: .5, type: ["water"] },
+    { name: "Pikachu", height: .4, type: ["electric"] }
+];
+document.write(pokemonList); ß
