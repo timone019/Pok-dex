c198032b1a14a846f756ebe8a1cb0c28e498f642
timone019 <143154960+timone019@users.noreply.github.com>
2023-10-11 16:34:49 -0700

    did a for each function instead of for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,25 @@
 let pokemonList = [
     { name: "Charmander", height: .6, type: ["fire"] },
-    { name: "Squirtle", height: .5, type: ["water", "shell"] },
-    { name: "Pikachu", height: .4, type: ["electric", "speed"] }
+    { name: "Squirtle", height: .5, type: ["water", " shell"] },
+    { name: "Pikachu", height: .4, type: ["electric", " speed"] }
 ];
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height > .5) {
-        document.write(pokemonList[i].name + "  " + "(height:" + "  " + pokemonList[i].height + " ) " + "- Wow, that\'s big!" + "<br>");
-    }
-    else {
-        document.write(pokemonList[i].name + "  " + "(height:" + "  " + pokemonList[i].height + " ) " + "<br>");
-    }
-}
+// old code for let 
+//     for (let i = 0; i < pokemonList.length; i++) {
+//     if (pokemonList[i].height > .5) {
+//         document.write(pokemonList[i].name + "  " + "(height:" + "  " + pokemonList[i].height + " ) " + "- Wow, that\'s big!" + "<br>");
+//     }
+//     else {
+//         document.write(pokemonList[i].name + "  " + "(height:" + "  " + pokemonList[i].height + " ) " + "<br>");
+//     }
+
+// for each 
+pokemonList.forEach(function (pokemon) {
+    document.write(pokemon.name + ' is ' + pokemon.height + ' ' + pokemon.type + '<br>');
+});
+
+
+
+
+
 
 
