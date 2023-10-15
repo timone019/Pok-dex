8cd852c4fd62613dfa4c61fbf0b305c44aae2219
timone019 <143154960+timone019@users.noreply.github.com>
2023-10-15 13:48:22 -0700

    1.5 JS complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,53 @@
-let pokemonList = [
-    { name: "Charmander", height: .6, type: ["fire"] },
-    { name: "Squirtle", height: .5, type: ["water", " shell"] },
-    { name: "Pikachu", height: .4, type: ["electric", " speed"] }
-];
-// old code for let 
-//     for (let i = 0; i < pokemonList.length; i++) {
-//     if (pokemonList[i].height > .5) {
-//         document.write(pokemonList[i].name + "  " + "(height:" + "  " + pokemonList[i].height + " ) " + "- Wow, that\'s big!" + "<br>");
-//     }
-//     else {
-//         document.write(pokemonList[i].name + "  " + "(height:" + "  " + pokemonList[i].height + " ) " + "<br>");
-//     }
+let pokemonRepository = (function () {
 
-// for each 
-pokemonList.forEach(function (pokemon) {
+    let pokemonList = [
+        { name: "Charmander", height: .6, type: ["fire"] },
+        { name: "Squirtle", height: .5, type: ["water", " shell"] },
+        { name: "Pikachu", height: .4, type: ["electric", " speed"] }
+    ];
+
+
+
+    // IIFE wrapped around existing code: see 1st line and below
+
+
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+
+
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
+
+
+// retrieve the pokemonList outside of scope using the getAll function
+let myPokemonList = pokemonRepository.getAll();
+
+
+// for Each 
+myPokemonList.forEach(function (pokemon) {
+    console.log(pokemon.name + ' is ' + pokemon.height + ' ' + pokemon.type + '<br>');
     document.write(pokemon.name + ' is ' + pokemon.height + ' ' + pokemon.type + '<br>');
 });
 
 
+// my notes for me
 
-
-
-
-
+  // old code for let
+    //     for (let i = 0; i < pokemonList.length; i++) {
+    //     if (pokemonList[i].height > .5) {
+    //         document.write(pokemonList[i].name + "  " + "(height:" + "  " + pokemonList[i].height + " ) " + "- Wow, that\'s big!" + "<br>");
+    //     }
+    //     else {
+    //         document.write(pokemonList[i].name + "  " + "(height:" + "  " + pokemonList[i].height + " ) " + "<br>");
+    //     }
